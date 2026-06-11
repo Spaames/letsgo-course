a3de8a1d6665b959a69c5a68db31b426753f40d0
Rémi DUPUY <rdu@Remis-MacBook-Pro.local>
2026-06-11 09:23:25 +0200

    test ssh

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -17,3 +17,8 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
+
+//quick test for ssh connextion
+func () test() {
+	
+}
